f08813f9959eb415a9761221eeef4863e0472ecd
User511A <48769117+User511A@users.noreply.github.com>
2021-11-30 15:30:25 +0500

    update main.py
    update text

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Hello, World! I am use Docker'
+    return 'Hello! I am running in a docker container' 
 
 if __name__ == '__main__': 
     app.run(host='0.0.0.0', port=5000)
